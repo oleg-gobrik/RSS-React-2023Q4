92cbb526e72c7eb3bafe2cc601e6895cef15eedc
oleg-gobrik <oleg.gobrik@gmail.com>
2023-11-04 11:04:15 +0300

    refactor: change component for loading process

--- src/components/CardListWrapper/CardListWrapper.tsx
+++ src/components/CardListWrapper/CardListWrapper.tsx
@@ -1,28 +1,22 @@
 import React from 'react';
 import styles from './CardListWrapper.module.css';
 import CardList from '../CardList/CardList';
-import Card from '../Card/Card';
 import Paginator from '../Paginator/Paginator';
 import { Props } from './types';
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 
 export default function CardListWrapper(props: Props) {
   const { isLoadingSearch, searchObject, changeItemsHandler } = props;
-  if (isLoadingSearch) {
-    return (
-      <Card>
-        <p>Please wait, uploading data.</p>
-      </Card>
-    );
-  } else {
-    return (
-      <section className={styles.cardListWrapper}>
-        <CardList searchObject={searchObject} />
-        <Paginator
-          onChangePageHandler={changeItemsHandler}
-          previousPage={searchObject.previous}
-          nextPage={searchObject.next}
-        />
-      </section>
-    );
-  }
+  return isLoadingSearch ? (
+    <LoadingSpinner />
+  ) : (
+    <section className={styles.cardListWrapper}>
+      <CardList searchObject={searchObject} />
+      <Paginator
+        onChangePageHandler={changeItemsHandler}
+        previousPage={searchObject.previous}
+        nextPage={searchObject.next}
+      />
+    </section>
+  );
 }
