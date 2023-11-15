40b6c6fa252116eba630c79c1f6b7a8cd79f5964
oleg-gobrik <oleg.gobrik@gmail.com>
2023-11-15 16:37:24 +0300

    feat: add test cases for application

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,31 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import App from './App';
+import { testMockPeople } from './test/TestData';
 
-test('Renders the main page', () => {
-  render(<App />);
+describe('App', () => {
+  const realFetch = global.fetch;
+  afterEach(() => {
+    global.fetch = realFetch;
+  });
 
-  expect(true).toBeTruthy();
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
+  test('Renders the main page', async () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(testMockPeople),
+      })
+    ) as jest.Mock;
+    const { container } = render(<App />);
+
+    expect(container.getElementsByClassName('spinnerContainer').length).toBe(1);
+    await waitFor(() => {
+      expect(container.getElementsByClassName('spinnerContainer').length).toBe(
+        0
+      );
+    });
+  });
 });
--- src/routes/RouterSearch.test.tsx
+++ src/routes/RouterSearch.test.tsx
@@ -1,34 +1,32 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { routerSearchConfig } from './RouterSearch';
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
-//import { testMockPerson } from '../test/TestData';
 
 describe('RouterSearch component', () => {
-  test('RouterSearch renders 404 Page Not Found.', async () => {
+  const realFetch = global.fetch;
+  afterEach(() => {
+    global.fetch = realFetch;
+  });
+
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
+  test('Should render 404 Page Not Found.', async () => {
+    const consoleSpy = jest.spyOn(global.console, 'warn').mockImplementation();
+    global.fetch = jest.fn(() => {
+      Promise.reject(new Error('Test'));
+    }) as jest.Mock;
     const router = createMemoryRouter(routerSearchConfig, {
       initialEntries: ['/posts'],
     });
+
     render(<RouterProvider router={router} />);
-    expect(screen.getByText('404: Page Not Found')).toBeInTheDocument();
-  });
 
-  test('RouterSearch renders with spinner', async () => {
-    const router = createMemoryRouter(routerSearchConfig);
-    const { container } = render(<RouterProvider router={router} />);
-    expect(container.getElementsByClassName('spinnerContainer').length).toBe(1);
-  });
-
-  test('Should renders spinner loading and after card with name with density 20', async () => {
-    const router = createMemoryRouter(routerSearchConfig);
-    const { container } = render(<RouterProvider router={router} />);
-
-    expect(container.getElementsByClassName('spinnerContainer').length).toBe(1);
-
-    //await screen.findByText(testMockPerson.name);
-    screen.debug();
-    // const quantityCards = container.getElementsByClassName('card');
-    // expect(quantityCards.length).toEqual(testMockPeople.count)
-    // expect(screen.getByText(testMockPerson.name)).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.getByText('404: Page Not Found')).toBeInTheDocument();
+    });
+    consoleSpy.mockClear();
   });
 });
