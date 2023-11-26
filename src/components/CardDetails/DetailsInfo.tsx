3fddad7d37ea57649411b528f6b9e59aecc21aba
oleg-gobrik <oleg.gobrik@gmail.com>
2023-11-27 00:23:18 +0300

    refactor: remove unused files

--- src/components/CardDetails/DetailsInfo.tsx
+++ src/components/CardDetails/DetailsInfo.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import Link from 'next/link';
 import styles from './CardDetails.module.css';
 import Button from '../Button/Button';
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
@@ -37,7 +37,7 @@ export default function DetailsInfo({ id }: { id: string }) {
         <div className={styles.detailsContainer}>
           <div className={styles.close}>
             <Button additionalClass={styles.closeButton}>
-              <Link className={styles.link} to="..">
+              <Link className={styles.link} href="..">
                 Close
               </Link>
             </Button>
