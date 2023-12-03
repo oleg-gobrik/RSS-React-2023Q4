a8dbe079271da24ea7d8a770bf23247e63ceb1b4
oleg-gobrik <oleg.gobrik@gmail.com>
2023-12-04 01:50:22 +0300

    refactor: remove comments and small fix

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,17 +1,11 @@
 import { NavLink, Outlet } from 'react-router-dom';
 
-// export async function loader() {
-//   //const contacts = await getContacts();
-//   return {};
-// }
-
 export default function Root() {
   return (
     <>
       <div>
         <h1>React Forms Task</h1>
         <nav>
-          {/* <NavLink to={`/`}>Main</NavLink> */}
           <NavLink to={`controlledForm`}>Controlled Form</NavLink>
           <NavLink to={`uncontrolledForm`}>Uncontrolled Form</NavLink>
         </nav>
